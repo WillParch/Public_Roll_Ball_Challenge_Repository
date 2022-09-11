3f83c3a747f491bad98fbb12972efb8e7d9c356d
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 10:24:53 +0000

    Sep 11, 2022, 6:24 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,7 +36,7 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if()
+        if(count >= 12)
     }
 
     void FixedUpdate()
