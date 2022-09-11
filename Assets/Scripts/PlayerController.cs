7bf3224fe1ea22a60f8a4cc6190f1252b11ed8e5
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 04:36:02 +0000

    Sep 11, 2022, 12:36 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
