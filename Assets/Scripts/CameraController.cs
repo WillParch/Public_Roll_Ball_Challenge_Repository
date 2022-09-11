e6663fe8f573a982cf8d6f7d89dc687789146c52
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 06:32:33 +0000

    Sep 11, 2022, 2:32 AM

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset
     }
 
     // Update is called once per frame
