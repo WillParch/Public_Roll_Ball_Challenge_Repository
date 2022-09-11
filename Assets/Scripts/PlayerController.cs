cbf647d653fb3d9de6915da6515b7b9351c45c5c
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 08:39:44 +0000

    Sep 11, 2022, 4:39 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,6 +34,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.CompareTag(""))
         other.gameObject.SetActive(false);
     }
 }
