0293899bb084dc82f9c667135fc7e9d8696e509b
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 02:08:00 +0000

    Sep 10, 2022, 10:08 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
