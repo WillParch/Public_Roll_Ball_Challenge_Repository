3b7f3603811254ef1a0358a2d38238f7705ba93c
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-11 06:16:48 +0000

    Sep 11, 2022, 2:16 AM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-   public float speed = 0; 
-    
+    public float speed = 0;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
-    }        
+    }
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
