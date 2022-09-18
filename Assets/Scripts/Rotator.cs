2244b057bc03b79a94b35fe8771411084e126882
WillParch <92686843+WillParch@users.noreply.github.com>
2022-09-18 01:19:34 +0000

    Sep 17, 2022, 9:19 PM

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,37 +5,37 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 // Transforms to act as start and end markers for the journey.
 {
-public Transform startMarker;
-public Transform endMarker;
+    public Transform startMarker;
+    public Transform endMarker;
 
 // Movement speed in units/sec.
-public float speed = 1.0F;
+    public float speed = 1.0F;
 
 // Time when the movement started.
-private float startTime;
+    private float startTime;
 
 // Total distance between the markers.
-private float journeyLength;
+    private float journeyLength;
 
-void Start()
+    void Start()
     {
     // Keep a note of the time the movement started.
-    startTime = Time.time;
+        startTime = Time.time;
 
     // Calculate the journey length.
-    journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
     }
 
 // Follows the target position like with a spring
-void Update()
+    void Update()
     {
     // Distance moved = time * speed.
-    float distCovered = (Time.time - startTime) * speed;
+        float distCovered = (Time.time - startTime) * speed;
 
     // Fraction of journey completed = current distance divided by total distance.
-    float fracJourney = distCovered / journeyLength;
+        float fracJourney = distCovered / journeyLength;
 
     // Set our position as a fraction of the distance between the markers and pingpong the movement.
-    transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
     }
 }
